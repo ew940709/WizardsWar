f0f3f9abaa9dfeb495710995b6983a84defdab6d
Katarzyna Konieczna <kmakonieczna@gmail.com>
2016-08-16 17:34:28 +0200

    add floor texture

--- scripts/controls.js
+++ scripts/controls.js
@@ -122,32 +122,40 @@
 
 				// floor
 
-				geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+				geometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
 				geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
-				for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
+				// for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
 
-					var vertex = geometry.vertices[ i ];
-					vertex.x += Math.random() * 20 - 10;
-					vertex.y += Math.random() * 2;
-					vertex.z += Math.random() * 20 - 10;
+					// var vertex = geometry.vertices[ i ];
+					// vertex.x += Math.random() * 20 - 10;
+					// vertex.y += Math.random() * 2;
+					// vertex.z += Math.random() * 20 - 10;
 
-				}
+				// }
 
-				for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+				// for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
 
-					var face = geometry.faces[ i ];
-					face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-					face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-					face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+					// var face = geometry.faces[ i ];
+					// face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+					// face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+					// face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
 
-				}
-
-				material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+				// }
+				var floorTex = THREE.ImageUtils.loadTexture("textures/stone.jpg");
+				floorTex.wrapS = floorTex.wrapT = THREE.RepeatWrapping;
+				floorTex.repeat.set(200,200);
+				material = new THREE.MeshPhongMaterial({color: 0x3c3c3c, map: floorTex});
 
 				mesh = new THREE.Mesh( geometry, material );
 				scene.add( mesh );
 
+				// var floorTex = THREE.ImageUtils.loadTexture("textures/floor-pavement.jpg")
+				// var plane = new THREE.Mesh(new THREE.CubeGeometry(200, 100, 0.1, 30), new THREE.MeshPhongMaterial({color: 0x3c3c3c, map: floorTex}));
+				// plane.position.y = -7.5;
+				// plane.rotation.x = -0.5 * Math.PI;
+				// scene.add(plane);
+				
 				// objects
 
 				geometry = new THREE.BoxGeometry( 20, 20, 20 );
